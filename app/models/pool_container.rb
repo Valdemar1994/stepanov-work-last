f3221679544c82f4e76ca95585376f9e54d44e26
Vadim <hersones21@gmail.com>
2022-12-06 17:12:38 +0300

    add filtering pool by grades and specialityes

--- app/models/pool_container.rb
+++ app/models/pool_container.rb
@@ -1,6 +1,22 @@
 class PoolContainer < ApplicationRecord
     
-    has_many :pools
-    belongs_to :user
+  has_many :pools
+  belongs_to :user
 
+  def filtered_pools(filter_params)
+    
+    grade_id = filter_params[:grade]
+    speciality_id = filter_params[:speciality]
+    
+    pools_scope = pools
+  
+    if grade_id.present? || speciality_id.present?
+      profile_scope = Profile.all
+
+      profile_scope = profile_scope.by_grade(grade_id) if grade_id.present?
+      profile_scope = profile_scope.by_speciality(speciality_id) if speciality_id.present?
+      pools_scope = pools_scope.where(profile_id: profile_scope.ids)
+    end
+    pools_scope
+  end
 end
